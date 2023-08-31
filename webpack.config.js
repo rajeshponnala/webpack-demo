82f4a24a3834fec25858dbbe890613ec3ce76ab1
rajeshponnala <rajeshreddyponnala@gmail.com>
2023-09-01 00:32:45 +0530

    Config dev server using webpack-plugin-serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
-const { mode } = require('webpack-nano/argv');
+const { mode } = require('webpack-nano/argv'); 
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
 module.exports = {
+    watch: mode === "development",
+    entry: ['webpack-plugin-serve/client', './src'],
     mode,
     plugins:[
-        new MiniHtmlWebpackPlugin({ context: { title: "Demo" } })
-    ]
+        new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
+        new WebpackPluginServe({
+            port: parseInt(process.env.port) || 8081,
+            static: "./dist",
+            liveReload: true,
+            waitForBuild: true
+        })
+    ],
 }
